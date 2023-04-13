d6bf1e008078b55779f711dc2838f93cde0dc6f0
evdoxiataka <evitaka@hotmail.com>
2023-04-14 00:37:59 +0100

    adds readme and data

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# thesis_examples
+# Drivers' Reaction Time Problem
+## Examples used in my PhD Thesis
+
+### Data
+The drivers' reaction time dataset used in these examples (*evaluation_sleepstudy.csv*) is from Belenky et al. (2003). 
+
+It can be retrieved from: https://benlambertdotcom.files.wordpress.com/2018/08/all_data.zip. 
+
+### Models
+The PyMC3 code for the probabilistic homogenous, heterogenous, and hierarchical models mentioned in Chapters 1, 2, and 3 can be found in the *driver_reaction_time.ipynb*.
--- visualization.py
+++ visualization.py
@@ -7,8 +7,8 @@ plt.rcParams.update({'font.size': 18})
 
 c1 = "blue"
 c2 = "orange"
-c3 = "green"
-c4 = "red"
+c3 = "limegreen"
+c4 = "mediumvioletred"
 
 def line_points(x, slope, intercept):
     return x, [slope*i + intercept for i in x]
@@ -33,22 +33,26 @@ def lreg_plot(reactions, slope, intercept):
     ax.spines['right'].set_visible(False)
     return ax
 
-def homogenous_models_outputs(trace, posterior_predictive, slope, intercept, rt):
-    fig, ax = plt.subplots(1, 4, sharey = False,figsize=(22, 5))
+def homogenous_models_outputs(trace, prior_predictive, posterior_predictive, slope, intercept, rt):
+    fig, ax = plt.subplots(1, 4, sharey = False,figsize=(21, 5))
     ##
     az.plot_dist(trace['a'], color=c1, ax=ax[0])
+    az.plot_dist(prior_predictive['a'], color=c3, ax=ax[0])
     ax[0].vlines(x = intercept, ymin = 0, ymax = 0.06, colors = c2)
 
     ##
-    az.plot_dist(trace['b'], color=c1, ax=ax[1], label="Probabilistic Model")
+    az.plot_dist(trace['b'], color=c1, ax=ax[1], label="Probabilistic Model Posterior")
+    az.plot_dist(prior_predictive['b'], color=c3, ax=ax[1], label="Probabilistic Model Prior")
     ax[1].vlines(x = slope, ymin = 0, ymax = 0.31, colors = c2, label = 'Non-Probabilistic Model')
-    ax[1].legend(loc='upper center', bbox_to_anchor=(0.55, 1.2))
+    ax[1].legend(loc='upper center', bbox_to_anchor=(0.55, 1.31))
 
     ##
     az.plot_dist(trace['s'], color=c1, ax=ax[2])
+    az.plot_dist(prior_predictive['s'], color=c3, ax=ax[2])
 
     ##
     az.plot_dist(posterior_predictive['rt'], color=c1, ax=ax[3])
+    az.plot_dist(prior_predictive['rt'], color=c3, ax=ax[3])
     ax[3].vlines(x = rt, ymin = 0, ymax = 0.007, colors = c2)
 
     ##
@@ -61,21 +65,24 @@ def homogenous_models_outputs(trace, posterior_predictive, slope, intercept, rt)
     ax[3].spines['top'].set_visible(False)
     ax[3].spines['right'].set_visible(False)
 
-    ax[0].set_ylabel("Probability")
-    ax[1].set_ylabel("Probability")
-    ax[2].set_ylabel("Probability")
-    ax[3].set_ylabel("Probability")
+    ax[0].set_ylabel("Probability Density")
+    ax[1].set_ylabel("Probability Density")
+    ax[2].set_ylabel("Probability Density")
+    ax[3].set_ylabel("Probability Density")
 
     ax[0].set_xlabel("a")
     ax[1].set_xlabel("b")
     ax[2].set_xlabel("s")
     ax[3].set_xlabel("rt")
 
-    plt.savefig('reaction_posterior.pdf', dpi=300, bbox_inches="tight", transparent=True)
+    plt.savefig('ch1_Fig2.pdf', dpi=300, bbox_inches="tight", transparent=True)
 
 def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predictive, posterior_predictive_het, posterior_predictive_hi, slope, intercept):
     fig, ax = plt.subplots(3, 4, sharey = False,figsize=(22, 15))
 
+    ##
+    subs = ['308','309','332','351']#337
+    subs_idx = [0,1,5,11]#9
     ##
     rt_hom_prob_ = np.array([posterior_predictive['rt'][:,[s*len(reactions.Days.unique())+day for s in range(len(reactions.Subject.unique()))]].flatten() for day in reactions.Days.unique()])
     rt_hom_prob = np.array([[rt_hom_prob_[:,draw] for draw in range(rt_hom_prob_.shape[1])]])
@@ -94,16 +101,16 @@ def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predict
     az.plot_dist(trace['a'], color=c1, ax=ax[0][3], label = "hom prob")
 
     ## heterogenous
-    az.plot_dist(trace_het['a'][:,0], color=c3, ax=ax[0][0])
-    az.plot_dist(trace_het['a'][:,1], color=c3, ax=ax[0][1])
-    az.plot_dist(trace_het['a'][:,3], color=c3, ax=ax[0][2])
-    az.plot_dist(trace_het['a'][:,9], color=c3, ax=ax[0][3], label = "het prob")
+    az.plot_dist(trace_het['a'][:,subs_idx[0]], color=c3, ax=ax[0][0])
+    az.plot_dist(trace_het['a'][:,subs_idx[1]], color=c3, ax=ax[0][1])
+    az.plot_dist(trace_het['a'][:,subs_idx[2]], color=c3, ax=ax[0][2])
+    az.plot_dist(trace_het['a'][:,subs_idx[3]], color=c3, ax=ax[0][3], label = "het prob")
 
     ## hierarchical
-    az.plot_dist(trace_hi['a'][:,0], color=c4, ax=ax[0][0])
-    az.plot_dist(trace_hi['a'][:,1], color=c4, ax=ax[0][1])
-    az.plot_dist(trace_hi['a'][:,3], color=c4, ax=ax[0][2])
-    az.plot_dist(trace_hi['a'][:,9], color=c4, ax=ax[0][3], label = "hi prob")
+    az.plot_dist(trace_hi['a'][:,subs_idx[0]], color=c4, ax=ax[0][0])
+    az.plot_dist(trace_hi['a'][:,subs_idx[1]], color=c4, ax=ax[0][1])
+    az.plot_dist(trace_hi['a'][:,subs_idx[2]], color=c4, ax=ax[0][2])
+    az.plot_dist(trace_hi['a'][:,subs_idx[3]], color=c4, ax=ax[0][3], label = "hi prob")
 
     ## b
     ## homogenous non-probabilistic
@@ -119,19 +126,19 @@ def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predict
     az.plot_dist(trace['b'], color=c1, ax=ax[1][3], label = "hom prob")
 
     ## heterogenous
-    az.plot_dist(trace_het['b'][:,0], color=c3, ax=ax[1][0])
-    az.plot_dist(trace_het['b'][:,1], color=c3, ax=ax[1][1])
-    az.plot_dist(trace_het['b'][:,3], color=c3, ax=ax[1][2])
-    az.plot_dist(trace_het['b'][:,9], color=c3, ax=ax[1][3], label = "het prob")
+    az.plot_dist(trace_het['b'][:,subs_idx[0]], color=c3, ax=ax[1][0])
+    az.plot_dist(trace_het['b'][:,subs_idx[1]], color=c3, ax=ax[1][1])
+    az.plot_dist(trace_het['b'][:,subs_idx[2]], color=c3, ax=ax[1][2])
+    az.plot_dist(trace_het['b'][:,subs_idx[3]], color=c3, ax=ax[1][3], label = "het prob")
 
     ## hierarchical
-    az.plot_dist(trace_hi['b'][:,0], color=c4, ax=ax[1][0])
-    az.plot_dist(trace_hi['b'][:,1], color=c4, ax=ax[1][1])
-    az.plot_dist(trace_hi['b'][:,3], color=c4, ax=ax[1][2])
-    az.plot_dist(trace_hi['b'][:,9], color=c4, ax=ax[1][3], label = "hi prob")
+    az.plot_dist(trace_hi['b'][:,subs_idx[0]], color=c4, ax=ax[1][0])
+    az.plot_dist(trace_hi['b'][:,subs_idx[1]], color=c4, ax=ax[1][1])
+    az.plot_dist(trace_hi['b'][:,subs_idx[2]], color=c4, ax=ax[1][2])
+    az.plot_dist(trace_hi['b'][:,subs_idx[3]], color=c4, ax=ax[1][3], label = "hi prob")
 
     ## regression lines
-    subs = ['308','309','330','337']
+    hdi_prob = 0.97
     subjects = reactions.Subject.unique()
     groups = reactions.groupby('Subject')
     for name,group in groups:
@@ -143,16 +150,16 @@ def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predict
             x,y = line_points(reactions.Days, slope, intercept)
             hdi_hom_np, = ax[2][idx].plot(x , y, c = c2, linestyle = '-', linewidth = 3, label = "hom non-prob")  
             ## homogenous prob
-            hdi_hom = az.plot_hdi(reactions.Days.unique(), rt_hom_prob, color = c1, hdi_prob=0.97, ax = ax[2][idx], plot_kwargs={'label':'hom prob','alpha':0.5}, fill_kwargs={'alpha': 0.1})
+            hdi_hom = az.plot_hdi(reactions.Days.unique(), rt_hom_prob, color = c1, hdi_prob=hdi_prob, ax = ax[2][idx], plot_kwargs={'label':'hom prob','alpha':0.5}, fill_kwargs={'alpha': 0.1})
             ## heterogenous prob
             idx_subjects = list(reactions.Subject.unique()).index(name)
             rt_het_prob_ = np.array([posterior_predictive_het['rt'][:,[s*len(reactions.Days.unique())+day for s in range(len(reactions.Subject.unique())) if s == idx_subjects]].flatten() for day in reactions.Days.unique()])
             rt_het_prob = np.array([[rt_het_prob_[:,draw] for draw in range(rt_het_prob_.shape[1])]])
-            hdi_het = az.plot_hdi(reactions.Days.unique(), rt_het_prob, color = c3, hdi_prob=0.97, ax = ax[2][idx], plot_kwargs={'label':'het prob','alpha':0.5}, fill_kwargs={'alpha': 0.1})
+            hdi_het = az.plot_hdi(reactions.Days.unique(), rt_het_prob, color = c3, hdi_prob=hdi_prob, ax = ax[2][idx], plot_kwargs={'label':'het prob','alpha':0.5}, fill_kwargs={'alpha': 0.1})
             ## hierarchical prob
             rt_hi_prob_ = np.array([posterior_predictive_hi['rt'][:,[s*len(reactions.Days.unique())+day for s in range(len(reactions.Subject.unique())) if s == idx_subjects]].flatten() for day in reactions.Days.unique()])
             rt_hi_prob = np.array([[rt_hi_prob_[:,draw] for draw in range(rt_hi_prob_.shape[1])]])
-            hdi_hi = az.plot_hdi(reactions.Days.unique(), rt_hi_prob, color = c4, hdi_prob=0.97, ax = ax[2][idx], plot_kwargs={'label':'hi prob','alpha':0.5}, fill_kwargs={'alpha': 0.1},backend_kwargs = {})
+            hdi_hi = az.plot_hdi(reactions.Days.unique(), rt_hi_prob, color = c4, hdi_prob=hdi_prob, ax = ax[2][idx], plot_kwargs={'label':'hi prob','alpha':0.5}, fill_kwargs={'alpha': 0.1},backend_kwargs = {})
 
     ##
     for i,r in enumerate(ax):
@@ -160,7 +167,7 @@ def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predict
             ax[i][j].spines['top'].set_visible(False)
             ax[i][j].spines['right'].set_visible(False)
             if i<2:
-                ax[i][j].set_ylabel("Probability")
+                ax[i][j].set_ylabel("Probability Density")
             else:
                 ax[i][j].set_ylabel("Reaction Time (ms)")
             if i == 0:
@@ -185,7 +192,7 @@ def hom_het_hi_reg_line(reactions, trace, trace_het, trace_hi, posterior_predict
                 else:
                     ax[i][j].get_legend().remove()
 
-    plt.savefig('reg_line_hom_het_hi.pdf', dpi=300, bbox_inches="tight", transparent=True)
+    plt.savefig('ch1_Fig3.pdf', dpi=300, bbox_inches="tight", transparent=True)
     
 def kruschke_style_posteriors(trace):
     ax = az.plot_posterior(trace, hdi_prob=0.97, textsize=20)
